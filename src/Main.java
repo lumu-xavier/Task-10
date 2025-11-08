3a3afbb56c4491c2766d729cc10b72ee79bcec1e
lumu-xavier <xavierfrancis391.@gmail.com>
2025-11-08 22:48:07 +0300

    commit2
    In the new version, i have created module-info.java file to export trialPackage, imported trialPackage in the main class on line 3, created a trialClass object on line 11 and called the trialMethod using the trialClass object.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import itemPackage.ItemManager;
 import userPackage.UserManager;
+import trialPackage.trialClass;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(" SIMPLE DATA MANAGEMENT APPLICATION \n");
         UserManager userManager = new UserManager();
         ItemManager itemManager = new ItemManager();
+        trialClass trialClass = new trialClass();
+
 
         userManager.inputUserData();
         itemManager.inputItemData();
 
         userManager.displayUserData();
         itemManager.displayItemData();
+lumu
+        trialClass.trialMethod();
     }
 }
---
+++ trialModule/src/module-info.java
@@ -0,0 +1,3 @@
+module trialModule {
+    exports  trialPackage;
+}
