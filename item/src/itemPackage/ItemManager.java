dada4313413a26941c7952cc7294c5b1a33dfa86
lumu-xavier <xavierfrancis391.@gmail.com>
2025-11-08 22:32:22 +0300

    commit2
    In the version, i have deleted the itemPrice variable on line 7, its input field on line 14 and 15, and its display statement on line 23.

--- item/src/itemPackage/ItemManager.java
+++ item/src/itemPackage/ItemManager.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class ItemManager {
     private String itemName;
-    private double itemPrice;
+
     private int itemQuantity;
 
     public void inputItemData() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the name of the item you want to add: ");
         itemName = input.nextLine();
-        System.out.print("Enter the price of the item you want to add: ");
-        itemPrice = input.nextDouble();
         System.out.print("Enter the quantity of the item you want to add: ");
         itemQuantity = input.nextInt();
     }
@@ -20,7 +18,6 @@ public class ItemManager {
     public void displayItemData() {
         System.out.println("\n=== ITEM INFO ===");
         System.out.println("Name: " + itemName);
-        System.out.println("Price: " + itemPrice);
         System.out.println("Quantity: " + itemQuantity);
     }
 }
