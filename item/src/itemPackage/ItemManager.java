f6e58629db8e6a03d05154998d01ff362423a8c0
lumu-xavier <xavierfrancis391.@gmail.com>
2025-11-08 22:29:11 +0300

    commit1
    In the new version, I have added a new variable itemQuantity on line 8, added lines 16 and 17 to allow the user to input Item quantity and also the display its output on line 24

--- item/src/itemPackage/ItemManager.java
+++ item/src/itemPackage/ItemManager.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ItemManager {
     private String itemName;
     private double itemPrice;
+    private int itemQuantity;
 
     public void inputItemData() {
         Scanner input = new Scanner(System.in);
@@ -12,11 +13,14 @@ public class ItemManager {
         itemName = input.nextLine();
         System.out.print("Enter the price of the item you want to add: ");
         itemPrice = input.nextDouble();
+        System.out.print("Enter the quantity of the item you want to add: ");
+        itemQuantity = input.nextInt();
     }
 
     public void displayItemData() {
         System.out.println("\n=== ITEM INFO ===");
         System.out.println("Name: " + itemName);
         System.out.println("Price: " + itemPrice);
+        System.out.println("Quantity: " + itemQuantity);
     }
 }
